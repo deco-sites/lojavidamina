5f17957ab888423ef655e3ec8ffa4005a25e8069
unify-master <gustavo@unify.eng.br>
2025-01-17 11:51:42 -0300

    add class to tailwind

--- sections/Footer/Footer.tsx
+++ sections/Footer/Footer.tsx
@@ -112,7 +112,7 @@ function Footer({
             <span class="text-xs font-normal text-base-400">{trademark}</span>
           </div>
 
-          <div style="justify-content:flex-end !important;z-index:9999999;" class="flex flex-nowrap items-center justify-center gap-4">
+          <div style="justify-content:flex-end !important;z-index:9999999;" class="about-us-link flex flex-nowrap items-center justify-center gap-4">
             <span class="text-sm font-normal text-base-400"><a href="/about-us">Quem somos</a></span>
             
           </div>
