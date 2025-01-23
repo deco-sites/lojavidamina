99b1f90b9c285e337f971a43091dd2852286f26a
unify-master <gustavo@unify.eng.br>
2025-01-23 15:29:58 -0300

    another form to use user

--- sections/Header/Header.tsx
+++ sections/Header/Header.tsx
@@ -92,8 +92,11 @@ const Desktop = ({ navItems, logo, searchbar, loading, user }: Props) => (
         </label>
 
         <div class="flex gap-4 place-self-end">
+          <p>
+            {user}
+          </p>
           <a href="/login">
-            <img src="https://data.decoassets.com/lojavidamina/d6ea5db3-5843-44e2-b7ad-d0a1880fc561/user-thin.png" alt={`${user.email}`} width="32" height="32" />
+            <img src="https://data.decoassets.com/lojavidamina/d6ea5db3-5843-44e2-b7ad-d0a1880fc561/user-thin.png" alt="login" width="32" height="32" />
           </a>
           <Bag />
         </div>
