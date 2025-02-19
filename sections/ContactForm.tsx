752eb2e08f7198816d7e9d17ad7f70e747839734
Gustavo Borges <gustavo@unify.eng.br>
2025-02-19 22:50:38 +0000

    Publish 2 from staging into origin/main

--- sections/ContactForm.tsx
+++ sections/ContactForm.tsx
@@ -1,3 +1,3 @@
 export default function Section() {
-  return <iframe style="width:100%;height:50vh;" src="https://materiais.usevidamina.com.br/contato" title="Contact Page"></iframe>
+  return <iframe style="width:100%;height:60vh;" scrolling="no" seamless="seamless" src="https://materiais.usevidamina.com.br/contato" title="Contact Page"></iframe>
 }
