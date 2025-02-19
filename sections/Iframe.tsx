6975ce86a911059429dad7d427e2d3b6a7992f6d
unify-master <gustavo@unify.eng.br>
2025-02-19 11:32:41 -0300

    create Iframe section

---
+++ sections/Iframe.tsx
@@ -0,0 +1,7 @@
+export default function Iframe() {
+    return (
+        <div>
+            <iframe src="https://www.usevidamina.com.br/login?returnUrl=/account" title="Login Page"></iframe>
+        </div>
+    );
+}
