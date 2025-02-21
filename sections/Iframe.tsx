c0f1c90298c305b9855354a345bc8ede2a41e530
unify-master <gustavo@unify.eng.br>
2025-02-21 10:09:28 -0300

    remove unused section

--- sections/Iframe.tsx
+++
@@ -1,7 +0,0 @@
-export default function Iframe() {
-    return (
-        <div>
-            <iframe src="https://www.usevidamina.com.br/login?returnUrl=/account" title="Login Page"></iframe>
-        </div>
-    );
-}
