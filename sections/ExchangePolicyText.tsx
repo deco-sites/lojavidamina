7e5ab70bcc17c1371e961a2419af222bc2ba75f1
Gustavo Borges <gustavo@unify.eng.br>
2025-02-26 16:25:52 +0000

    Publish 4 from staging into origin/main

---
+++ sections/ExchangePolicyText.tsx
@@ -0,0 +1,57 @@
+export default function Section() {
+  return <div class="exchange-policy">
+  <h2>Considerações Gerais</h2>
+
+<p>Você sabia que pode trocar ou desistir da sua compra?</p>
+
+<p>Após a entrega, você tem até 7 (sete) dias corridos para cancelar ou trocar o produto. Esse é um direito que está no artigo 49 do Código de Defesa do Consumidor.</p>
+
+<p>Para solicitar a troca ou devolução entre em contato através do Email: contato@usevidamina.com.br.</p>
+
+<p>Informando: <b>Motivo da Troca ou Cancelamento e Dados da Compra</b></p>
+
+<p>Nosso objetivo é garantir a sua satisfação e o seu bem-estar. A seguir, seguem as regras das políticas e em quais situações poderão utilizá-la:</p>
+
+<h3>Troca ou Devolução</h3>
+
+<p>a) A troca ou devolução do seu produto só será realizada se o produto estiver na embalagem original, sem violação do lacre e sem sinais de uso.</p>
+<p>b) Assim que o produto estiver embalado, você pode entrar em contato com a nossa Central de Atendimento e solicitar a troca. Será gerado um código de postagem e você deve comparecer em uma agência dos correios e fazer o envio do produto sem custo. Assim que o produto voltar ao Centro de Distribuição da Vidamina, uma equipe fará uma triagem do produto. Essa análise tem o prazo de 7 (sete) dias úteis.</p>
+<p>c) O produto será analisado e se não estiver de acordo com as regras citadas acima, ele poderá ser devolvido sem aviso prévio.</p>
+<p>d) Assim que você fizer a solicitação de troca ou devolução você receberá um e-mail com os prazos, pois pode variar conforme a sua região.</p>
+
+<h3>Arrependimento de compra</h3>
+
+<p>O cliente tem o direito de desistir da compra, realizando a devolução do pedido. Para tal, é necessário seguir as regras abaixo:</p>
+
+<ul>
+    <li>Entre em contato através do e-mail: contato@usevidamina.com.br no prazo máximo de 07 (sete) dias corridos da data de recebimento do produto.</li>
+    <li>O produto deverá ser devolvido e enviado junto com a Nota Fiscal do pedido.</li>
+    <li>Você pode optar pela troca do item comprado, por um produto igual ao adquirido ou de mesmo valor.</li>
+    <li>E caso não se interesse pela troca, estornaremos os valores pagos.</li>
+</ul>
+
+<h3>Produto com avaria de transporte e pedido violado</h3>
+
+<p>Para os casos em que o produto esteja com defeito de fábrica ou qualquer outro problema que impeça o seu uso correto:</p>
+
+<ul>
+    <li>Confira no ato da entrega se a embalagem está violada ou existe algum produto com avaria, se houver, recuse o pedido e anote na Nota Fiscal o motivo da devolução.</li>
+</ul>
+
+<h3>Prazo de envio do novo produto</h3>
+
+<p>O envio do novo produto é o último passo da troca.</p>
+
+<p>Assim que o produto é devolvido ao nosso estoque, ele passa por uma análise de 7 (sete) dias úteis e a nova venda será gerada no sistema. Você receberá em seu e-mail a Nota fiscal do novo produto e a partir daí, o prazo de entrega é o mesmo que você aguardou no ato da compra.</p>
+
+<p><b>Atenção: O prazo de entrega do novo produto só será o mesmo que você aguardou no ato da compra, se o endereço solicitado na troca for o mesmo inserido na compra. Se você alterou o endereço de entrega, o prazo pode variar de acordo com a região.</b></p>
+
+<h3>Estorno do valor pago conforme o meio de pagamento</h3>
+
+<h4>Cartão de crédito</h4>
+
+<p>Para devolução de valores dos pedidos pagos com cartão de crédito será efetuado o estorno/cancelamento total ou parcial (se somente um dos produtos for devolvido) da transação junto à administradora do cartão de crédito.</p>
+
+<p>Após a solicitação de estorno/cancelamento realizado por esta empresa, o crédito será identificado pela administradora do cartão de crédito em até três faturas após a conclusão da devolução, dependendo da data de fechamento da fatura do cartão de crédito.</p>
+  </div>
+}
